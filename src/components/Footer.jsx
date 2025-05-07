c5252a34113c42dc97280384ead78d6c8620a2ff
Utku Şen <utkusen9@gmail.com>
2025-05-08 00:11:53 +0300

    hot fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -75,7 +75,7 @@ function Footer() {
                                 <svg className="w-5 h-5 mr-3 mt-0.5 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                 </svg>
-                                <span>+49 157 5782 9251</span>
+                                <span>+49 123 456 789</span>
                             </li>
                             <li className="flex items-start">
                                 <svg className="w-5 h-5 mr-3 mt-0.5 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
